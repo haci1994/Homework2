811b850fad775aac04f31b9da7fc921bd4eed86c
haci1994 <mmzadj@gmail.com>
2025-01-29 12:00:10 +0400

    1st exercise

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////1. Massivin cəmini tapın.
+
+//Console.WriteLine("MASSIVIN CEMINI HESABLAYAN PROQRAM");
+
+//Console.Write("Massiviniz neche elementden ibaretdir? ");
+//int n = int.Parse(Console.ReadLine());
+//int sum = 0;
+
+//Console.WriteLine($"{n} sayda eded daxil edin:");
+
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write($"Eded {i + 1}:");
+//    int a = int.Parse(Console.ReadLine());
+//    sum += a;
+//}
+
+//Console.WriteLine($"Massive daxil etdiyiniz ededlerin cemi = {sum}");
+
+//Console.WriteLine("---------------------");
+
+
+
